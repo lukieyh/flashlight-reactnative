f21291ad994ac1d1d36bdb595e933d04235a3131
unknown <lukiey.git@gmail.com>
2022-06-28 21:12:19 -0300

    Flashlight-reactnative-bootcamp

---
+++ App.js
@@ -0,0 +1,67 @@
+import React, {useState, useEffect} from 'react'
+import {View, StyleSheet, Image, TouchableOpacity, Alert} from 'react-native'
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
+
+const App = ()=>{
+  const [toggle, setToggle] = useState(false);
+  
+  const handChangeToggle = () => setToggle(oldToggle => !oldToggle);
+  useEffect(()=> {
+    // Liga flash do celular
+    Torch.switchState(toggle);
+    console.log('trocou estado do flash');
+  }, [toggle]);
+
+useEffect(() => {
+  const subscription = RNShake.addListener(()=>{
+    setToggle(oldToggle => !oldToggle);
+  });
+  
+  return () => subscription.remove();
+  });
+
+  return <View style={toggle ? style.containerLight : style.container} >
+    <TouchableOpacity onPress={handChangeToggle}>
+    <Image style={toggle ? style.lightingOn : style.lightingOff} source={toggle ? require('./assets/icons/eco-light.png') : require('./assets/icons/eco-light-off.png') } />
+    <Image style={style.dioLogo} source={toggle ? require('./assets/icons/logo-dio.png') : require('./assets/icons/logo-dio-white.png') } />
+    </TouchableOpacity>
+    </View>
+};
+export default App;
+
+const style = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerLight:{
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  lightingOn:{
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    wigth: 150,
+    height: 150,
+  },
+  lightingOff:{
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    tintColor: 'white',
+    wigth: 150,
+    height: 150,
+  },
+  dioLogo:{
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    wigth: 100,
+    height: 100,
+    
+  },
+})
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # flashlight-reactnative
 APP FlashLight React Native
+## Conteúdo
+
+- Libs react-native-shake e react-native-torch
+- StyleSheet
+- Hook useState
+- Hook useEffect
+- Add Listener to RNShake
+- Lifecicly ReactJS
